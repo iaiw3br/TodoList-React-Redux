3aa02142d45a6537b3a1999b27af69545018beb5
ArtemIvanov <iaiw3br@gmail.com>
2020-01-29 22:54:18 +0300

    change type files from .js to .jsx

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -5,7 +5,7 @@ const Link = ({ active, children, onClick }) => (
   <button
     onClick={onClick}
     disabled={active}
-    style={{ marginLeft: '4px' }}
+    type="submit"
   >
     {children}
   </button>
